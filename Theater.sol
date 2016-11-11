115bf7a43fdf70a6b44cfbfb8c9a1781f355ec46
sibizulu <sibiraj@lightrains.com>
2016-11-11 10:14:14 +0530

    Adding theater booking contract

---
+++ Theater.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.0;
+
+contract Theater{
+
+    address public owner;
+    mapping (address => uint) public balances;
+    uint public totalSeats;
+    uint public bookCount;
+
+    function Theater(){
+      owner = msg.sender;
+      totalSeats = 500;
+      bookCount = 0;
+    }
+
+    function buyTicket() public returns (bool success){
+      if (bookCount >= totalSeats) return false;
+      balances[msg.sender] = msg.value;
+      bookCount++;
+      return true;
+    }
+
+    function refundTicket(address reciever, uint amt) public {
+      if (msg.sender != owner) return;
+      if (balances[reciever] == amt){
+          address contractAddress = this;
+          if (contractAddress.balance >= amt) {
+            reciever.send(amt);
+            balances[reciever] = 0;
+            bookCount--;
+          }
+      }
+    }
+}
